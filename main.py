2f3dbdc42d1255cee1717412cb6eefd9a940eb7d
VilMe <vilma.medrano00@gmail.com>
2025-11-08 21:34:08 -0600

    finish defining bet match function and start defining chat bot function

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from difflib import get_close_matches
 
 def get_best_match(user_question: str, questions: dict) -> str | None:
     questions: list[str] = [q for q in questions]
-    matches: list = get_close_matches(user_question, questions, n=1)
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6) # can play around with cutoff for how strict the bot will be
+
+
+    if matches:
+        return matches[0]
+    
+
+def chat_bot(knowledge: dict):
