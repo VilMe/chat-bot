ed92f80e56f81509b415a4a5f707dec5afc44640
VilMe <vilma.medrano00@gmail.com>
2025-11-08 21:30:12 -0600

    continue to define get best match functions

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from difflib import get_close_matches
 
 
 def get_best_match(user_question: str, questions: dict) -> str | None:
-    
+    questions: list[str] = [q for q in questions]
+    matches: list = get_close_matches(user_question, questions, n=1)
